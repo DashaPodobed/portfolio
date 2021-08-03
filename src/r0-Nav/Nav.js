2df9e66257af470c70b26c81c4f755c95e2e3b25
DariaPodobed <dariamakarchik@gmail.com>
2021-08-03 18:58:35 +0300

    LinkComponent

--- src/r0-Nav/Nav.js
+++ src/r0-Nav/Nav.js
@@ -3,44 +3,25 @@ import style from './Nav.module.scss'
 import {Link} from 'react-scroll'
 
 const Nav = () => {
+    const LinkComponent = (props) => {
+        return (
+            <Link
+                activeClass={style.active}
+                to={props.path}
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+            >{props.title}
+            </Link>
+        )
+    }
     return (
         <div className={style.nav}>
-            <Link
-                activeClass={style.active}
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-            >Home
-            </Link>
-            <Link
-                activeClass={style.active}
-                to="skills"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-            >Skills
-            </Link>
-            <Link
-                activeClass={style.active}
-                  to="projects"
-                  spy={true}
-                  smooth={true}
-                  offset={0}
-                  duration={500}
-            >Projects
-            </Link>
-            <Link
-                activeClass={style.active}
-                  to="contacts"
-                  spy={true}
-                  smooth={true}
-                  offset={0}
-                  duration={500}
-            >Contacts
-            </Link>
+            <LinkComponent path={"home"} title={"Home"}/>
+            <LinkComponent path={"skills"} title={"Skills"}/>
+            <LinkComponent path={"projects"} title={"Projects"}/>
+            <LinkComponent path={"contacts"} title={"Contacts"}/>
         </div>
     );
 }
