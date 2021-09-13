8970ed47d12805797fec399adedd622fc25d7fea
DariaPodobed <dariamakarchik@gmail.com>
2021-09-13 17:19:31 +0300

    "fixed form"

--- src/r6-Contacts/Contacts.js
+++ src/r6-Contacts/Contacts.js
@@ -5,16 +5,50 @@ import Fade from 'react-reveal/Fade';
 
 
 const Contacts = () => {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('name:', name);
+        console.log('email:', email);
+        console.log('message:', message);
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
+
     return (
         <div id="contacts" className={style.contactsBlock}>
             <div className={style.contactsContainer}>
                 <Title title={"Contacts"}/>
                 <Fade>
-                    <form className={style.form}>
-                        <input type="text" className={style.formArea} placeholder="Name"/>
-                        <input type="text" className={style.formArea} placeholder="e-mail"/>
-                        <textarea className={style.messageArea} placeholder="Message"/>
-                        <button className={style.button}>Send</button>
+                    <form className={style.form} onSubmit={handleSubmit}>
+                        <input
+                            placeholder="Name"
+                            className={style.formArea}
+                            id="name"
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <input
+                            placeholder="Email"
+                            className={style.formArea}
+                            id="email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <textarea
+                            placeholder="Message"
+                            className={style.messageArea}
+                            id="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <button className={style.button} type="submit">Send</button>
                     </form>
                 </Fade>
             </div>
